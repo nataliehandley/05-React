ed08e2db62c3168bc1a879ec768ed0db0aa4075b
natalie-coder <63603124+natalie-coder@users.noreply.github.com>
2021-12-28 21:15:33 +0000

    add styling

--- ticket-tracker/src/components/Counter/Counter.jsx
+++ ticket-tracker/src/components/Counter/Counter.jsx
@@ -8,8 +8,8 @@ const Counter = () => {
     const [count, setCount] = useState(0);
     return (
         <div className = {styles.counter}>
-            <h1>Counter</h1>
-            <h1>{count}</h1>
+            <h2>Counter</h2>
+            <p>{count}</p>
             <div className = {styles.buttonStyles}>
                 <button className = {styles.button} onClick={() => setCount(count-1)}>-</button>
                 <button className = {styles.button} onClick={() => setCount(count+1)}>+</button>
